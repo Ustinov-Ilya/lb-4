dba58d3a55b762b3103f10e448861fa92c742636
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-05 17:10:13 +0300

    completed 3 exercise

--- solutions/3-spread-operator.js
+++ solutions/3-spread-operator.js
@@ -1,3 +1,13 @@
 // BEGIN
+export const dateConvert = (...arrs) => {
+    if (arrs.length === 0) return [];
+    let result = [];
+    for (let arr of arrs) {
+        const date = new Date(...arr);
+        result.push(date.toDateString()); 
+    }
+    return result;
+}
 
+export default dateConvert;
 // END
