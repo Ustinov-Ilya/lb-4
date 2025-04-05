4025d1930989c0a6ecd6edb80a9888957032ec26
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-05 17:58:13 +0300

    completed 4 exercise

--- solutions/4-first-class-citizen.js
+++ solutions/4-first-class-citizen.js
@@ -1,6 +1,13 @@
 const run = (text) => {
     // BEGIN
-
+    const takeLast = (str, n) => {
+        if (str.length < n) return null;
+        let result = '';
+        for (let i = 0; i < n; i++) {
+            result += str[str.length - 1 - i];
+        }
+        return result;
+    }
     // END
   
     return takeLast(text, 4);
