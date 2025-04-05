0d1793eb875904cc5b872798fcb35684a0a7ff6a
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-05 16:51:24 +0300

    completed 2 exercise

--- solutions/2-rest-opertator.js
+++ solutions/2-rest-opertator.js
@@ -1,5 +1,12 @@
 import _ from 'lodash';
 
 // BEGIN
+export const arithmeticMean = (...numbers) => {
+    if (numbers.length === 0) return null;
+    let sum = _.sum(numbers);
+    let result = sum / numbers.length;
+    return result;
+}
 
+export default arithmeticMean;
 // END
