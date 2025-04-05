ba37dfe8a57c3e8067cf3d2bf4b9a73a305aa420
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-05 16:35:54 +0300

    completed 1 exercise

--- solutions/1-pure-functions.js
+++ solutions/1-pure-functions.js
@@ -1,3 +1,25 @@
 // BEGIN
+const isPrime = (namber) => {
+    if (namber < 2) {
+        return false;
+    }
+    if (namber === 2) {
+        return true;
+    }
+    if (namber % 2 === 0) {
+        return false;
+    }
+    for (let i = 3; i <= Math.sqrt(namber); i+= 2) {
+        if (namber % i === 0) {
+            return false;
+        } 
+    }
+    return true;
+}
 
+export const sayPrimeOrNot = (nam) => {
+    console.log(isPrime(nam) ? 'yes' : 'no');
+}
+
+export default sayPrimeOrNot;
 // END
