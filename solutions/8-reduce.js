57d655a3d59c41f0802b186b0014aee707fa5dcb
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-07 20:47:48 +0300

    completed 8 exercise

--- solutions/8-reduce.js
+++ solutions/8-reduce.js
@@ -1,3 +1,15 @@
 // BEGIN
+export const groupBy = (arr, property) => {
+    const group = (acc, object) => {
+        if (!Object.hasOwn(acc, object[property])) {
+            acc[object[property]] = [];
+        }
+        acc[object[property]].push(object);
+        return acc;
+    }
+    const groupedObjects = arr.reduce(group, {});
+    return groupedObjects;
+}
 
+export default groupBy;
 // END
