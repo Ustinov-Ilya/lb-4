3a82e99ca194d490b648c87f164dea39a9152220
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-07 20:47:38 +0300

    completed 7 exercise

--- solutions/7-filter.js
+++ solutions/7-filter.js
@@ -1,3 +1,9 @@
 // BEGIN
+export const getGirlFriends = (users) => {
+    const friends = users.map((user) => user.friends).flat(Infinity);
+    const girlFriends = friends.filter((friend) => friend.gender === 'female');
+    return girlFriends;
+}
 
+export default getGirlFriends;
 // END
