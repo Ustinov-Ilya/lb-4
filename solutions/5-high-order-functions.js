0ad65b2f2faed9d4678cdbadc350661048ec9970
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-07 20:47:15 +0300

    completed 5 exercise

--- solutions/5-high-order-functions.js
+++ solutions/5-high-order-functions.js
@@ -1,5 +1,9 @@
-import _ from 'lodash';
+import _, { sortBy } from 'lodash';
 
 // BEGIN
+export const takeOldest = (users, count = 1) => {
+    return sortBy(users, ({birthday}) => Date.parse(birthday)).slice(0, count);
+}
 
+export default takeOldest;
 // END
