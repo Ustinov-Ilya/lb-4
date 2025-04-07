b5e2e2fe697382f40da1667babfffdcbbe9bd940
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-07 20:47:28 +0300

    completed 6 exercise

--- solutions/6-map.js
+++ solutions/6-map.js
@@ -1,3 +1,8 @@
 // BEGIN
+export const getChildren = (users) => {
+    const childrens = users.map((user) => user.children);
+    return childrens.flat(Infinity);
+}
 
+export default getChildren;
 // END
