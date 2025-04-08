b39d9f156c0966a4a7ba2379a4f0cd19fc1e30ed
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-08 10:11:57 +0300

    completed 9 exercise

--- solutions/9-signals.js
+++ solutions/9-signals.js
@@ -8,5 +8,18 @@ const freeEmailDomains = [
 ];
 
 // BEGIN
+const domenCount = (acc, domen) =>{
+  if(!acc[domen]){
+    acc[domen] = 0;
+  }
+  acc[domen] = acc[domen] + 1;
+  return acc;
+}
 
+const getFreeDomainsCount = (emails) => emails
+.map((domen) => domen.split('@')[1])
+.filter((email) => freeEmailDomains.includes(email))
+.reduce(domenCount, {});
+
+export default getFreeDomainsCount;
 // END
