39087541d8186ce1c17811d288440ded796d6192
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-04-08 13:31:08 +0300

    completed 11 exercise

--- solutions/11-iterative.js
+++ solutions/11-iterative.js
@@ -1,6 +1,15 @@
 const smallestDivisor = (num) => {
     // BEGIN
+    if (num === 1) return 1;
 
+    const iter = (divisible, divider) => {
+        if (divisible === divider || divisible % divider === 0) {
+            return divider;
+        }
+        return iter(divisible, divider + 1);
+    }
+
+    return iter(num, 2);
     // END
   };
   
